cbe7457ea9dc4a0b09a85f7914ab0181f626f77c
cuongphamhuy <kts.mungtoi@gmail.com>
2020-05-10 13:37:24 +0700

    add-relation

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 from apps.base.models import TimeStampMixin
 
 class Project(TimeStampMixin, models.Model):
@@ -20,6 +22,9 @@ class Team(TimeStampMixin, models.Model):
   abbreviation = models.CharField(max_length=255)
   description = models.TextField()
 
+  projects = models.ManyToManyField(Project)
+  members = models.ManyToManyField(User)
+
   def __str__(self):
     return self.title
 
@@ -30,6 +35,8 @@ class Position(TimeStampMixin, models.Model):
   title = models.CharField(max_length=255)
   abbreviation = models.CharField(max_length=255)
   description = models.TextField()
+
+  members = models.ManyToManyField(User)
 
   def __str__(self):
     return self.title
@@ -43,6 +50,8 @@ class Skill(TimeStampMixin, models.Model):
   description = models.TextField()
 
   used_date = models.DateField()
+
+  members = models.ManyToManyField(User)
 
   def __str__(self):
     return self.title
