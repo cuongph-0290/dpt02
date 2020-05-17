15d3767e4d478d1a1fac2a327c00ce09f8ff1375
cuongphamhuy <kts.mungtoi@gmail.com>
2020-05-17 13:27:10 +0700

    seperate-right-side-bar

--- apps/static/javascripts/admin/RightSideBar.js
+++ apps/static/javascripts/admin/RightSideBar.js
@@ -1,25 +1,17 @@
 import Component from '../shared/Component.js'
+import SearchTime from './right_side_bar/SearchTime.js'
+import SearchFullModal from './right_side_bar/SearchFullModal.js'
 
 export default class RightSideBar extends Component {
   prepareChildElements() {
-    const $this = this
-
-    this.element.querySelector('div.last-week').onclick = () =>
-      $this.props.handleSearch({
-        created_at__gt: (new Date(new Date()
-          .setDate(new Date().getDate() - 7))).toISOString().split('T')[0]
+    this.childElements.searchTime = new SearchTime(
+      this.element.querySelector('.search-time'), {
+        handleSearch: this.props.handleSearch
       })
 
-    this.element.querySelector('div.last-month').onclick = () =>
-      $this.props.handleSearch({
-        created_at__gt: (new Date(new Date()
-          .setMonth(new Date().getMonth() - 1))).toISOString().split('T')[0]
-      })
-
-    this.element.querySelector('div.last-year').onclick = () =>
-      $this.props.handleSearch({
-        created_at__gt: (new Date(new Date()
-          .setYear(new Date().getFullYear() - 1))).toISOString().split('T')[0]
+    this.childElements.searchFullModal = new SearchFullModal(
+      this.element.querySelector('.search-full-modal'), {
+        handleSearch: this.props.handleSearch
       })
   }
 
@@ -35,35 +27,13 @@ export default class RightSideBar extends Component {
             <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 011.415 0l3.85 3.85a1 1 0 01-1.414 1.415l-3.85-3.85a1 1 0 010-1.415z" clip-rule="evenodd"/>
             <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zM13 6.5a6.5 6.5 0 11-13 0 6.5 6.5 0 0113 0z" clip-rule="evenodd"/>
           </svg>
-          <div class="mt-2 dropdown-menu display-none" onclick=clearStyle(this)>
-            <div class="p-3">
-              <div class="form-group">
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="title or description contain">
-              </div>
-              <div class="form-group">
-                <select class="form-control">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </select>
-              </div>
-              <div class="form-group">
-                <input type="text" class="form-control" id="exampleInputPassword1" placeholder="start date">
-              </div>
-              <div class="form-group">
-                <input type="text" class="form-control" id="exampleInputPassword1" placeholder="end date">
-              </div>
-              <button class="btn btn-primary">Submit</button>
-            </div>
+          <div class="mt-2 dropdown-menu display-none search-full-modal">
           </div>
         </div>
       </div>
       <br><br><br>
-      <div class="float-right pr-3 last-week clickable">Last week</div>
-      <div class="float-right pr-3 last-month clickable">Last month</div>
-      <div class="float-right pr-3 last-year clickable">Last year</div>
+      <div class="search-time">
+      </div>
     `
   }
 }
